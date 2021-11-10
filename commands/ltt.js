63571b7b891a5407a0a75a03efa9fa8b22bb66c1
Felix Hillebrand <97hilfel@gmail.com>
2021-11-10 12:35:45 +0100

    Add two example commands

--- commands/ltt.js
+++ commands/ltt.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('serverinfo')
-        .setDescription('Display info about this server.'),
+        .setName('ltt')
+        .setDescription('Plays a random ltt advert in voice chat.'),
     async execute(interaction) {
         return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
     },
